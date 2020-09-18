8e424721d7332dfae73c9bfca03a9c371b7db5f4
Sam Wood <16246185+sw517@users.noreply.github.com>
2020-09-18 15:40:14 +0100

    Feature - adds transition to table

--- src/components/InteractiveTable.vue
+++ src/components/InteractiveTable.vue
@@ -9,68 +9,66 @@
         <th>Preferred Pet</th>
         <th>Preferred Fruit</th>
       </thead>
-      <tbody>
-        <template v-for="(person, index) in people">
-          <tr v-if="showTableRow(person)" :key="index">
-            <td>
-              <input
-                :value="person.name"
-                type="text"
-                class="input input--name"
-                @input="onInputChange($event, index, 'name')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-            <td>
-              <input
-                :value="person.age"
-                type="number"
-                min="0"
-                max="999"
-                class="input input--age"
-                @input="onInputChange($event, index, 'age')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-            <td>
-              <input
-                :value="person.gender"
-                type="text"
-                class="input input--gender"
-                @input="onInputChange($event, index, 'gender')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-            <td>
-              <input
-                :value="person.eyeColor"
-                type="text"
-                class="input input--eye-color"
-                @input="onInputChange($event, index, 'eyeColor')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-            <td>
-              <input
-                :value="person.preferences.pet"
-                type="text"
-                class="input input--pet"
-                @input="onInputChange($event, index, 'preferences.pet')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-            <td>
-              <input
-                :value="person.preferences.fruit"
-                type="text"
-                class="input input--fruit"
-                @input="onInputChange($event, index, 'preferences.fruit')"
-                @keydown.enter="onKeyDownEnter"
-              />
-            </td>
-          </tr>
-        </template>
-      </tbody>
+      <transition-group tag="tbody" name="list-swap">
+        <tr v-for="(person, index) in cPeople" :key="person.name">
+          <td>
+            <input
+              :value="person.name"
+              type="text"
+              class="input input--name"
+              @input="onInputChange($event, index, 'name')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+          <td>
+            <input
+              :value="person.age"
+              type="number"
+              min="0"
+              max="999"
+              class="input input--age"
+              @input="onInputChange($event, index, 'age')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+          <td>
+            <input
+              :value="person.gender"
+              type="text"
+              class="input input--gender"
+              @input="onInputChange($event, index, 'gender')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+          <td>
+            <input
+              :value="person.eyeColor"
+              type="text"
+              class="input input--eye-color"
+              @input="onInputChange($event, index, 'eyeColor')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+          <td>
+            <input
+              :value="person.preferences.pet"
+              type="text"
+              class="input input--pet"
+              @input="onInputChange($event, index, 'preferences.pet')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+          <td>
+            <input
+              :value="person.preferences.fruit"
+              type="text"
+              class="input input--fruit"
+              @input="onInputChange($event, index, 'preferences.fruit')"
+              @keydown.enter="onKeyDownEnter"
+            />
+          </td>
+        </tr>
+      </transition-group>
     </table>
   </div>
 </template>
@@ -87,6 +85,33 @@ export default {
   },
   computed: {
     ...mapGetters(['people']),
+    cPeople() {
+      /**
+       * Show table row if the searchString prop matches any
+       * substring of one of its cell's values.
+       */
+      let people = this.people;
+      if (!this.searchString) return people;
+
+      const search = this.searchString.toLowerCase();
+
+      return people.reduce((acc, person) => {
+        const {
+          name,
+          gender,
+          eyeColor,
+          age,
+          preferences: { fruit, pet },
+        } = person;
+
+        const include = [name, gender, eyeColor, age, fruit, pet].some((value) =>
+          String(value).toLowerCase().includes(search)
+        );
+
+        if (include) acc.push(person);
+        return acc;
+      }, []);
+    },
   },
   methods: {
     ...mapActions(['setPersonValue']),
@@ -103,27 +128,6 @@ export default {
     blurInput() {
       document.activeElement.blur();
     },
-    /**
-     * Show table row if the searchString prop matches any
-     * substring of one of its cell's values.
-     * @param {object} person An object from the 'people' array
-     * in the state in store.
-     * @return {boolean} Whether the row should be visible.
-     */
-    showTableRow(person) {
-      if (!this.searchString) return true;
-      const search = this.searchString.toLowerCase();
-      const {
-        name,
-        gender,
-        eyeColor,
-        age,
-        preferences: { fruit, pet },
-      } = person;
-      return [name, gender, eyeColor, age, fruit, pet].some((value) =>
-        String(value).toLowerCase().includes(search)
-      );
-    },
   },
 };
 </script>
@@ -139,9 +143,8 @@ td {
 }
 
 tr {
-  &:nth-child(even) {
-    @apply bg-gray-200;
-  }
+  box-shadow: 0 -1px 0 0 theme('colors.accent-light');
+
   &:hover,
   &:focus-within {
     @apply bg-accent-light;
@@ -172,5 +175,24 @@ tr {
     @apply bg-white;
     cursor: text;
   }
+}
+
+.list-swap-enter-active,
+.list-swap-leave-active {
+  transition: all ease 0.4s;
+}
+
+.list-swap-enter,
+.list-swap-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.list-swap-leave-active {
+  position: absolute;
+}
+
+.list-swap-move {
+  transition: transform 1s ease;
 }
 </style>
